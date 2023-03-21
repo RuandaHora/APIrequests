99aa4ec9bcbc3c3d6c5be0c17843de77a7379e7f
Ruan da Hora <ruanpablosnake@gmail.com>
2023-03-21 20:50:34 -0300

    Initial Commit

---
+++ APIrequests/APIrequestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIrequestsApp.swift
+//  APIrequests
+//
+//  Created by Marcelo Hora on 21/03/23.
+//
+
+import SwiftUI
+
+@main
+struct APIrequestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
