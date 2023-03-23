cf84ae70441d94b116fce5a08ecdf56a3baa54a2
Ruan da Hora <ruanpablosnake@gmail.com>
2023-03-23 20:36:05 -0300

    Update

---
+++ APIrequests/View/Screens/Settings.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+struct Settings: View {
+    var body: some View {
+        NavigationView{
+            VStack{
+                List{
+                    
+                    HStack {
+                        VStack (alignment: .leading){
+                            HStack{
+                                Image("Ruan")
+                                    .resizable()
+                                    .frame(width: 60, height: 60)
+                                    .cornerRadius(100)
+                                Text("Ruan Pablo Silva da Hora")
+                                    .font(.system(size: 20))
+                                    .foregroundColor(Color.primary)
+                               
+                            }
+                            Text("                  Configurações da conta")
+                                .font(.system(size: 12))
+                                .foregroundColor(Color.primary)
+                        }
+                        Spacer()
+                        Image(systemName: "chevron.forward")
+                            .font(.system(size: 15))
+                            .foregroundColor(Color.secondary)
+                    }
+                    HStack{
+                        Text("Sujestões de ID Apple")
+                    }
+                }
+            }.navigationTitle("Settings")
+        }
+    }
+}
+
+struct Settings_Previews: PreviewProvider {
+    static var previews: some View {
+        Settings()
+    }
+}
