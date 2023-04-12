87a6cbc837b20f662083b2edfc254b8fbcf84667
Ruan da Hora <ruanpablosnake@gmail.com>
2023-04-12 20:08:11 -0300

    ScrollView Gustavo

--- APIrequests/View/Shared/MVVMteste.swift
+++ APIrequests/View/Shared/MVVMteste.swift
@@ -21,45 +21,47 @@ struct gustavoFlow: View {
     @Binding var viewModel: profileViewModel
     
     var body: some View {
-        VStack{
-            Image(viewModel.user.picture)
-                .resizable()
-                .frame(width: 250, height: 250)
-                .cornerRadius(150)
-                .padding(10)
-            
-            Text(viewModel.user.name)
-                .font(.system(size: 40, weight: .bold))
-                .foregroundColor(.primary)
-            
-            Text(viewModel.user.nick)
-                .font(.system(size: 22, weight: .regular))
-                .foregroundColor(.secondary)
-            
-            Text("\(viewModel.userFollowers)")
-                .font(.system(size: 75, weight: .light))
-                .foregroundColor(.primary)
+        ScrollView(showsIndicators: false) {
             VStack{
-                //Seguir
-                Button{viewModel.isFollowing.toggle()} label: {
-                    Label(!viewModel.isFollowing ? "Seguir" : "Seguindo", systemImage:!viewModel.isFollowing
-                            ? "person.crop.circle.fill.badge.plus" : "checkmark.seal.fill")
-                        .font(.title2)
-                        .frame(maxWidth: .infinity)
-                }.buttonStyle(.borderedProminent)
-                    .controlSize(.large)
-                    .tint(!viewModel.isFollowing ? .blue : .black)
-                //Mensagem
-                NavigationLink{MsPrivado1()} label: {
-                    Label("Mensagem", systemImage: "paperplane")
-                        .font(.title2)
-                        .frame(maxWidth: .infinity)
-                }.buttonStyle(.borderedProminent)
-                    .controlSize(.large)
-                    .disabled(!viewModel.isFollowing)
-                    .tint(!viewModel.isFollowing ? .blue : .yellow)
-            }.padding(20)
-            
+                Image(viewModel.user.picture)
+                    .resizable()
+                    .frame(width: 250, height: 250)
+                    .cornerRadius(150)
+                    .padding(10)
+                
+                Text(viewModel.user.name)
+                    .font(.system(size: 40, weight: .bold))
+                    .foregroundColor(.primary)
+                
+                Text(viewModel.user.nick)
+                    .font(.system(size: 22, weight: .regular))
+                    .foregroundColor(.secondary)
+                
+                Text("\(viewModel.userFollowers)")
+                    .font(.system(size: 75, weight: .light))
+                    .foregroundColor(.primary)
+                VStack{
+                    //Seguir
+                    Button{viewModel.isFollowing.toggle()} label: {
+                        Label(!viewModel.isFollowing ? "Seguir" : "Seguindo", systemImage:!viewModel.isFollowing
+                                ? "person.crop.circle.fill.badge.plus" : "checkmark.seal.fill")
+                            .font(.title2)
+                            .frame(maxWidth: .infinity)
+                    }.buttonStyle(.borderedProminent)
+                        .controlSize(.large)
+                        .tint(!viewModel.isFollowing ? .blue : .black)
+                    //Mensagem
+                    NavigationLink{MsPrivado1()} label: {
+                        Label("Mensagem", systemImage: "paperplane")
+                            .font(.title2)
+                            .frame(maxWidth: .infinity)
+                    }.buttonStyle(.borderedProminent)
+                        .controlSize(.large)
+                        .disabled(!viewModel.isFollowing)
+                        .tint(!viewModel.isFollowing ? .blue : .yellow)
+                }.padding(20)
+                
+            }
         }
     }
 }
