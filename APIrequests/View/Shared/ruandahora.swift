6eef605596ef966648e67fd38548a59b234a1fa8
Ruan da Hora <ruanpablosnake@gmail.com>
2023-04-12 20:02:19 -0300

    ScrollView ruan

--- APIrequests/View/Shared/ruandahora.swift
+++ APIrequests/View/Shared/ruandahora.swift
@@ -19,44 +19,46 @@ struct ruanFlow: View {
     @State var viewModel = ruanViewModel()
     
     var body: some View {
-        VStack{
-            Image("ruan 1")
-                .resizable()
-                .frame(width: 250, height: 250)
-                .cornerRadius(150)
-                .padding(10)
-            
-            Text(viewModel.user.name)
-                .font(.system(size: 40, weight: .bold))
-                .foregroundColor(.primary)
-            
-            Text(viewModel.user.nick)
-                .font(.system(size: 22, weight: .regular))
-                .foregroundColor(.secondary)
-            
-            Text(viewModel.userFollowers)
-                .font(.system(size: 75, weight: .light))
-                .foregroundColor(.primary)
-            
+        ScrollView {
             VStack{
-                //Seguir
-                Button{viewModel.isFollowing.toggle()} label: {
-                    Label(!viewModel.isFollowing ? "Seguir" : "Seguindo", systemImage:!viewModel.isFollowing
-                            ? "person.crop.circle.fill.badge.plus" : "checkmark.seal.fill")
-                        .font(.title2)
-                        .frame(maxWidth: .infinity)
-                }.buttonStyle(.borderedProminent)
-                    .controlSize(.large)
-                    .tint(!viewModel.isFollowing ? .blue : .black)
-                Button{} label: {
-                    Label("Mensagem", systemImage: "paperplane")
-                        .font(.title2)
-                        .frame(maxWidth: .infinity)
-                }.buttonStyle(.borderedProminent)
-                    .controlSize(.large)
-                    .disabled(!viewModel.isFollowing)
-                    .tint(!viewModel.isFollowing ? .blue : .yellow)
-            }.padding(20)
+                Image("ruan 1")
+                    .resizable()
+                    .frame(width: 250, height: 250)
+                    .cornerRadius(150)
+                    .padding(10)
+                
+                Text(viewModel.user.name)
+                    .font(.system(size: 40, weight: .bold))
+                    .foregroundColor(.primary)
+                
+                Text(viewModel.user.nick)
+                    .font(.system(size: 22, weight: .regular))
+                    .foregroundColor(.secondary)
+                
+                Text(viewModel.userFollowers)
+                    .font(.system(size: 75, weight: .light))
+                    .foregroundColor(.primary)
+                
+                VStack{
+                    //Seguir
+                    Button{viewModel.isFollowing.toggle()} label: {
+                        Label(!viewModel.isFollowing ? "Seguir" : "Seguindo", systemImage:!viewModel.isFollowing
+                                ? "person.crop.circle.fill.badge.plus" : "checkmark.seal.fill")
+                            .font(.title2)
+                            .frame(maxWidth: .infinity)
+                    }.buttonStyle(.borderedProminent)
+                        .controlSize(.large)
+                        .tint(!viewModel.isFollowing ? .blue : .black)
+                    Button{} label: {
+                        Label("Mensagem", systemImage: "paperplane")
+                            .font(.title2)
+                            .frame(maxWidth: .infinity)
+                    }.buttonStyle(.borderedProminent)
+                        .controlSize(.large)
+                        .disabled(!viewModel.isFollowing)
+                        .tint(!viewModel.isFollowing ? .blue : .yellow)
+                }.padding(20)
+            }
         }
     }
 }
